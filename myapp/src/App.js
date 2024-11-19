9c6ea62bc5b745143f89559e97f31332e44820a5
piyumalBhathiya <139671996+piyumalBhathiya@users.noreply.github.com>
2024-11-19 17:33:56 +0000

    adding font size change button and finalizing the project

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,22 +1,38 @@
-import './App.css';
-import {students} from './Data/StudentsDb';
-import StudentTable from './Components/StudentTable'
+import "./App.css";
+import { useState } from "react";
+import { students } from "./Data/StudentsDb";
+import StudentTable from "./Components/StudentTable";
 
 function App() {
+  const [fontSize, setFontSize] = useState(16);
+
+  const changeFontSize = (size) => {
+    if (size === "S") setFontSize(12);
+    else if (size === "M") setFontSize(16);
+    else if (size === "L") setFontSize(20);
+  };
+
   return (
-    <div className="App">
-      <h1>Students Information Portal</h1>
-      <table width='100%'>
-        <tbody>
-          <tr>
-            <td>
-              <StudentTable students={students}/>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    
+    <>
+      <div className="App" style={{ fontSize: `${fontSize}px` }}>
+        <h1>Students Information Portal</h1>
+        <div className="fontSize">
+          Font Size :<button onClick={() => changeFontSize("S")}>S</button>
+          <button onClick={() => changeFontSize("M")}>M</button>
+          <button onClick={() => changeFontSize("L")}>L</button>
+        </div>
+
+        <table width="100%">
+          <tbody>
+            <tr>
+              <td>
+                <StudentTable students={students} className={fontSize} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
--- myapp/src/Components/Profile.js
+++ myapp/src/Components/Profile.js
@@ -1,33 +1,40 @@
-export default function Profile(props){
-    return(
-        <>
-            <table>
-                <tr>
-                    <td rowspan={7}>
-                        <img className='profile' src={require('../Assets/img'+props.stu.ProfilePic)} alt="Profile Picture" />
-                    </td>
-                    <td>ID: {props.stu.studentId}</td>
-                </tr>
-                <tr>
-                    <td>Full Name: {props.stu.firstName + ' ' + props.stu.lastName}</td>
-                </tr>
-                <tr>
-                    <td>Age: {props.stu.age}</td>
-                </tr>
-                <tr>
-                    <td>Course: {props.stu.course}</td>
-                </tr>
-                <tr>
-                    <td>Gender: {props.stu.gender}</td>
-                </tr>
-                <tr>
-                    <td>Adderss: {props.stu.address.city + ' ' + props.stu.address.country}</td>
-                </tr>
-                <tr>
-                    <td>Skills: {props.stu.address.city.map(skill=>skill+=', ')}</td>
-                </tr>
-
-            </table>
-        </>
-    );
+import "../Assets/css/compo.css";
+export default function Profile(props) {
+  console.log(props.stu);
+  return (
+    <>
+      <table>
+        <tr>
+          <td rowspan={7}>
+            <img
+              className="profile"
+              src={require("../Assets/img/" + props.stu.profilePic)}
+              alt="profile"
+            />
+          </td>
+          <td>ID: {props.stu.studentId}</td>
+        </tr>
+        <tr>
+          <td>Full Name: {props.stu.firstName + " " + props.stu.lastName}</td>
+        </tr>
+        <tr>
+          <td>Age: {props.stu.age}</td>
+        </tr>
+        <tr>
+          <td>Course: {props.stu.course}</td>
+        </tr>
+        <tr>
+          <td>Gender: {props.stu.gender}</td>
+        </tr>
+        <tr>
+          <td>
+            Adderss: {props.stu.address.city + " " + props.stu.address.country}
+          </td>
+        </tr>
+        <tr>
+          <td>Skills: {props.stu.skills.map((skill) => (skill += ", "))}</td>
+        </tr>
+      </table>
+    </>
+  );
 }
