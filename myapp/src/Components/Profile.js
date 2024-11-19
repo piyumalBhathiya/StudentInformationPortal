5e3432f4810362db6bf395fb6ad0f306812835db
piyumalBhathiya <139671996+piyumalBhathiya@users.noreply.github.com>
2024-11-19 16:25:16 +0000

    Profile layout and code is done

--- myapp/src/Components/Profile.js
+++ myapp/src/Components/Profile.js
@@ -4,10 +4,29 @@ export default function Profile(props){
             <table>
                 <tr>
                     <td rowspan={7}>
-                        <img className='profile' src={require('../Assets/img'+props.student.ProfilePic)} alt='Profile Picture' />
+                        <img className='profile' src={require('../Assets/img'+props.stu.ProfilePic)} alt="Profile Picture" />
                     </td>
-                    <td>ID: {props.student.studentId}</td>
+                    <td>ID: {props.stu.studentId}</td>
                 </tr>
+                <tr>
+                    <td>Full Name: {props.stu.firstName + ' ' + props.stu.lastName}</td>
+                </tr>
+                <tr>
+                    <td>Age: {props.stu.age}</td>
+                </tr>
+                <tr>
+                    <td>Course: {props.stu.course}</td>
+                </tr>
+                <tr>
+                    <td>Gender: {props.stu.gender}</td>
+                </tr>
+                <tr>
+                    <td>Adderss: {props.stu.address.city + ' ' + props.stu.address.country}</td>
+                </tr>
+                <tr>
+                    <td>Skills: {props.stu.address.city.map(skill=>skill+=', ')}</td>
+                </tr>
+
             </table>
         </>
     );
